1ef2b439fc1359793ced841030d4b3814f961a1b
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-01-17 09:40:09 +0300

    Binary Search

---
+++ BinarySearch/src/binarySearch/BinarySearch.java
@@ -0,0 +1,35 @@
+package binarySearch;
+
+public class BinarySearch {
+
+	public static int binarySearch(int[] arr, int target, int left, int right) {
+		if (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (arr[mid] == target) {
+				return arr[mid];
+			} else if (arr[mid] < target) {
+				return binarySearch(arr, target, mid + 1, right);
+			} else {
+				return binarySearch(arr, target, left, mid - 1);
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int target = 10;
+
+		int result = binarySearch(arr, target, 0, arr.length - 1);
+
+		if (result == -1) {
+			System.out.println("Target " + target + " is not found in the array");
+		} else {
+			System.out.println("Target " + target + " is found in array");
+		}
+
+	}
+
+}
